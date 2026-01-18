c8c8bf9fb0e35138e6a06fbd27fd0ee080194014
shahid <shelltroecnec@gmail.com>
2026-01-18 07:00:03 +0530

    chore : Updated Project firmware base structure. In this commit, I have added the files that I missed to add to staging. And setting  up final Project structural Architecture.

--- main/main.c
+++ main/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <esp_log.h>
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include "esp_utils.h"
+#include "lora.h"
+#include "ssd1306.h"
+#include "sx127x.h"
+
+SSD1306_t oled;
+static const char *TAG = "sx127x_rx";
+sx127x device;
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Farm-Gateway starting...");
 
+    // OLED init
+    i2c_master_init(&oled, GPIO_NUM_18, GPIO_NUM_17, -1);
+    ssd1306_init(&oled, 128, 64);
+    ssd1306_clear_screen(&oled, false);
+    ssd1306_contrast(&oled, 0xff);
+    ssd1306_display_text(&oled, 0, "LoRa RX Ready", 13, false);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    lora_init();
 }
